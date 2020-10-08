0a4a056fd65cd2410046fa9c88ca12304d7750d0
Jack <herndel300@gmail.com>
2020-10-08 00:57:40 -0600

    comment fix

--- script.js
+++ script.js
@@ -13,19 +13,25 @@ document.getElementById("btnLetsFindOut").addEventListener("click", function () 
     function FizzBuzzA(value1, value2) {
         //variable is equal to an empty string.      
         let returnValue = [];
-        //Need to create a "for" loop(aka:i). (let i=1; makes it start at zero, then i<=100;i++ tells it to stop once it is less than or equal to 100. but everytime it loops, it adds one to i (i++)       
+        //Need to create a "for" loop(aka:i). 
+        //(let i=1; makes it start at zero, then i<=100;i++ tells it to stop once it is less than or equal to 100. 
+        //but everytime it loops, it adds one to i (i++)       
         for (let i = 1; i <= 100; i++) {      
             //Use modulus operator to determine if number is divisible by 3 or 5. 
-            // (or entry1 entry2). == does not assign a value to something, but compares the values and return true or false. && means "and also".            
+            // (or entry1 entry2). == does not assign a value to something, 
+            //but compares the values and return true or false. && means "and also".            
             if (i % value1 == 0 && i % value2 == 0) {
-                //We're doing this in a loop, so += (incrementally) will add one iteration of itself (1) until it reaches it's parameters (<=100). if both of those == 0, then the string 'FizzBuzz ' is returned                       
+                //We're doing this in a loop, so += (incrementally) will add one iteration of itself (1) until it reaches it's parameters (<=100). 
+                // if both of those == 0, then the string 'FizzBuzz ' is returned                       
                 returnValue.push('FizzBuzz ');    
             }
-            //else if is saying "if the iteration (1-100) is divisible by value1 and has a remainder of zero, it will evaluate to true and returnValue will return one iteration of itself and evaluate to 'Fizz '            
+            //else if is saying "if the iteration (1-100) is divisible by value1 and has a remainder of zero, 
+            //it will evaluate to true and returnValue will return one iteration of itself and evaluate to 'Fizz '            
             else if (i % value1 == 0) {
                 returnValue.push('Fizz '); 
             }
-            //else if is saying "if the iteration (1-100)'s is divisible by value2 and has a remainder of zero, it will evaluate to true and returnValue will return one iteration of itself and evaluate to 'Buzz '            
+            //else if is saying "if the iteration (1-100)'s is divisible by value2 and has a remainder of zero, 
+            //it will evaluate to true and returnValue will return one iteration of itself and evaluate to 'Buzz '            
             else if (i % value2 == 0) {   
                 returnValue.push('Buzz ');
             }
